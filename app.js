e6a8646fa9ca209a621872c2bcaddc0c25a7a13e
Luke Schlangen <lukeschlangen@gmail.com>
2017-08-02 15:05:25 -0500

    Adding get request for geese array

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+// This array will not be here when we add a database
+// that is where data wshould be based
+var geeseArray = [
+    {
+        name: 'Fred',
+        age: 5,
+        type: 'Betelgeusian Goosian'
+    },
+    {
+        name: 'Carla',
+        age: 4,
+        type: 'Canadian'
+    }
+];
+
 var express = require('express');
 
 var app = express();
@@ -5,6 +20,10 @@ var app = express();
 var port = 5000;
 
 app.use(express.static('public'));
+
+app.get('/geese', function(req, res){
+    res.send(geeseArray);
+});
 
 app.listen(port, function(){
     console.log('Running on port', port);
